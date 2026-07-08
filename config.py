572b7dfa522192e942735cb80a897425fbd7a33f
prerna245 <prernaraj2266@gmail.com>
2026-07-08 22:08:54 +0530

    Initialize config.py with application settings
    Added configuration settings for email, origins, keys, and limits.

---
+++ config.py
@@ -0,0 +1,31 @@
+EMAIL = "24f2007562@ds.study.iitm.ac.in"
+
+Q1_ALLOWED_ORIGIN = "https://dash-lxq1km.example.com"
+
+ISSUER = "https://idp.exam.local"
+AUDIENCE = "tds-ymyrk1dq.apps.exam.local"
+
+PUBLIC_KEY_PEM = """-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA2okOHspNjgA+2rTLbeuY
+cxiP/hG8C6Sb9iwg3yiLAA4HCnpITcbWCSelbvbYGuc3EbNy4xFyf5Cbj5DHJMID
+EkryOgyd2giIIIBOUBj8S63uGcnRpOBh9NFatfNwheKuzsPuVNldu6A9cNteNpXc
+WyJjG2axVfmq7i6SuKr1JoWYG7xTTAvKPujSl4OtsQfO3h5NepzdfXpr28oNnzfW
+ed+zclR6BcmNNo/WVfJ4xyCLSf0BCOgdTgW6PdaChd1l9VDetJZVEgC5tkyvXsfI
+SI6iyrYbKR0NEBSqq4XkadEjsCs4F1RncsS4LlgniT7GlkL9Mce3b0wGLs9/7ZIX
+dQIDAQAB
+-----END PUBLIC KEY-----"""
+
+Q3_PORT = 8000
+Q3_WORKERS = 1
+Q3_DEBUG = False
+Q3_LOG_LEVEL = "info"
+
+Q5_API_KEY = "ak_iioar1gdonhjkqwrvasjvbwu"
+
+Q9_TOTAL_ORDERS = 50
+Q9_RATE_LIMIT = 20
+
+Q10_ALLOWED_ORIGIN = "https://app-jyqe7m.example.com"
+Q10_RATE_LIMIT = 8
+
+EXAM_PORTAL_ORIGIN = "https://exam.sanand.workers.dev"
